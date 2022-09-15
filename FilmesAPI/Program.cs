09ef52bc3c8647f676a116043534920cf324b38b
ElmineiroStone <paulo.jesus@stone.com.br>
2022-09-15 16:12:05 -0300

    Feat: Insert mapper in API

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -9,7 +9,7 @@ string mySqlConnection =
 
 builder.Services.AddDbContextPool<FilmeContext>(options =>
 options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
